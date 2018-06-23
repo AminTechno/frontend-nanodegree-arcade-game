5cedf21110cff10f3162ea93ba0dc5bd339488aa
bluessista <inga@bluessista.com>
2018-06-23 11:49:27 +0200

    update methods of player and enemy

--- js/app.js
+++ js/app.js
@@ -7,34 +7,48 @@ class Enemy {
     // a helper we've provided to easily load images
     constructor (x, y, speed) {
         this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
     }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+    };
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-update (dt) => {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-render () => {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor(x, y) {
+        this.sprite = 'image/char-cat-horn-girl.png';
+        this.x = x;
+        this.y = y;
+    }
 
+    update() {}
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput() {}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+const allEnemies = [];
 // Place the player object in a variable called player
-
-
+const player = new Player(10, 10);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
